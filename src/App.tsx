69b5dd9340fab7c244a576c6ec52ed83d8c24677
maxitabakman <49928297@est.ort.edu.ar>
2026-08-31 09:05:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import RenderizadoCondicional from './RenderizadoCondicional';
+
+export default function App() {
+  return (
+    <main className="app-container">
+     <RenderizadoCondicional/>
+    </main>
+  );
+}
